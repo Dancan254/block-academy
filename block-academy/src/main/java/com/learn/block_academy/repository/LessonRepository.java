7cd0d4436ccf773c176ffac47a4448e4cf42dcd5
Dancan254 <dancanian25@gmail.com>
2025-03-14 14:09:02 +0300

    remove unused JpaSpecificationExecutor import from LessonRepository

--- block-academy/src/main/java/com/learn/block_academy/repository/LessonRepository.java
+++ block-academy/src/main/java/com/learn/block_academy/repository/LessonRepository.java
@@ -2,7 +2,5 @@ package com.learn.block_academy.repository;
 
 import com.learn.block_academy.model.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
 }
